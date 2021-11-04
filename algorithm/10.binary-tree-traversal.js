843418905205a31c682cfe849c9d57dfddc78ff1
allenl <liangle8090@gmail.com>
2021-11-04 22:22:02 +0800

    add:增加二叉树遍历

---
+++ algorithm/10.binary-tree-traversal.js
@@ -0,0 +1,77 @@
+/**
+ * 二叉树前序遍历
+ * @param {*} root 
+ * @param {*} arr 
+ * @returns 
+ */
+function preorderTraversal(root, arr) {
+  if (!root) return
+  arr.push(root.val)
+  preorderTraversal(root.left, arr)
+  preorderTraversal(root.right, arr)
+}
+
+/**
+ * 中序遍历，中序遍历后的结果是个有序数列
+ * @param {*} root 
+ * @param {*} arr 
+ */
+function inorderTraversal(root, arr) {
+  if (!root) return
+  inorderTraversal(root.left, arr)
+  arr.push(root.val)
+  inorderTraversal(root.right, arr)
+}
+
+/**
+ * 后序遍历
+ * @param {*} root 
+ * @param {*} arr 
+ */
+function postorderTraversal(root, arr) {
+  if (!root) return
+  postorderTraversal(root.left, arr)
+  postorderTraversal(root.right, arr)
+  arr.push(root.val)
+}
+
+const arr = [5, 3, 4, 2, 7, 6, 8]
+const bt = convertBinaryTree(arr)
+let tmp = []
+preorderTraversal(bt, tmp)
+console.log('前序遍历：', tmp.join(','))
+tmp = []
+inorderTraversal(bt, tmp)
+console.log('中序遍历：', tmp.join(','))
+tmp = []
+postorderTraversal(bt, tmp)
+console.log('后序遍历：', tmp.join(','))
+
+
+function convertBinaryTree(arr) {
+  let root;
+
+  let insertNode = function (parentNode, childNode) {
+    if (!childNode.val || childNode.val === '') return
+    if (childNode.val < parentNode.val) {
+      if (parentNode.left === null) parentNode.left = childNode
+      else insertNode(parentNode.left, childNode)
+    } else {
+      if (parentNode.right === null) parentNode.right = childNode
+      else insertNode(parentNode.right, childNode)
+    }
+  }
+
+  arr.forEach(val => {
+    let node = {
+      val: val,
+      left: null,
+      right: null
+    }
+
+    if (root) insertNode(root, node)
+    else root = node
+  });
+
+  return root
+}
