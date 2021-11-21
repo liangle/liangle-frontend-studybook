70ee2f45e9721a94c2113ce31df9f0183082b890
allenl <liangle8090@gmail.com>
2021-11-21 10:38:22 +0800

    feat:增加合并多个升序链表

---
+++ algorithm/42.w0403-mergeKLists.js
@@ -0,0 +1,22 @@
+function mergeKLists(lists) {
+  // return lists.reduce((prev, curr) => mergeTwoList(prev, curr))
+  const len = lists.length
+  let tmp = null
+  for (let i = 0; i < len; i++) {
+    tmp = mergeTwoList(tmp, lists[i])
+  }
+  return tmp
+}
+
+function mergeTwoList(l1, l2) {
+  if (!l1) return l2
+  if (!l2) return l1
+
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoList(l1.next, l2)
+    return l1
+  } else {
+    l2.next = mergeTwoList(l2.next, l1)
+    return l2
+  }
+}
