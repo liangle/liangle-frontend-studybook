ff81882946209d4a1f3206c8e8b4ada02a8c4bc1
allenl <liangle8090@gmail.com>
2021-11-28 18:24:12 +0800

    update:优化分隔链表代码

--- algorithm/54.w0503-partition.js
+++ algorithm/54.w0503-partition.js
@@ -14,21 +14,19 @@ function partition(head) {
     if (curr.val < x) {
       //当前节点的值小于x时
       //1.当前节点加到较小链表的尾部
-      //2.当前节点后移
-      //3.较小链表尾节点后移，并和下个节点断开
+      //2.较小链表尾节点后移
       samllEnd.next = curr
-      curr = curr.next
       samllEnd = samllEnd.next
-      samllEnd.next = null
     } else {
       largeEnd.next = curr
-      curr = curr.next
       largeEnd = largeEnd.next
-      largeEnd.next = null
     }
+    curr = curr.next
   }
 
   //把小链表的最后一个节点指向大链表真实的头节点
   samllEnd.next = largeHead.next
+  //更新最大尾节点的 next 为 null，防止出现环
+  largeEnd.next = null
   return smallHead.next
 }
