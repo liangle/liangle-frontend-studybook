dd0daa262b93c094e6d671d504fb218d404b5797
allenl <liangle8090@gmail.com>
2021-12-28 22:58:30 +0800

    增加347. 前 K 个高频元素

---
+++ algorithm/115.w0903-topKFrequent.js
@@ -0,0 +1,28 @@
+/**
+ * [347. 前 K 个高频元素](https://leetcode-cn.com/problems/top-k-frequent-elements/)
+ */
+var topKFrequent = function (nums, k) {
+  const map = new Map()
+  nums.forEach(num => {
+    map.set(num, map.has(num) ? map.get(num) + 1 : 1)
+  })
+
+  const minQueue = new MinPriorityQueue()
+  for ([key, value] of map) {
+    if (minQueue.size() < k) {
+      minQueue.enqueue(key, value)
+    } else {
+      if (minQueue.front()['priority'] < value) {
+        minQueue.dequeue()
+        minQueue.enqueue(key, value)
+      }
+    }
+  }
+
+  const ans = []
+  while (!minQueue.isEmpty()) {
+    ans.unshift(minQueue.dequeue()['element'])
+  }
+
+  return ans
+};
