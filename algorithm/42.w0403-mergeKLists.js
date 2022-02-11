c0fddc3e9055a961c427ee64b7e07c170b1f8b71
allenl <liangle8090@gmail.com>
2022-02-11 21:45:33 +0800

    refactor: 优化代码

--- algorithm/42.w0403-mergeKLists.js
+++ algorithm/42.w0403-mergeKLists.js
@@ -3,25 +3,30 @@
  * @param {*} lists 
  * @returns 
  */
-function mergeKLists(lists) {
-  // return lists.reduce((prev, curr) => mergeTwoList(prev, curr))
-  const len = lists.length
-  let tmp = null
-  for (let i = 0; i < len; i++) {
-    tmp = mergeTwoList(tmp, lists[i])
-  }
-  return tmp
-}
+var mergeKLists = function (lists) {
+    //创建小顶堆
+    const q = new MinPriorityQueue()
 
-function mergeTwoList(l1, l2) {
-  if (!l1) return l2
-  if (!l2) return l1
+    //将所有不为空的链表入小顶堆
+    lists.forEach(list => {
+        if (list) q.enqueue(list, list.val)
+    })
 
-  if (l1.val < l2.val) {
-    l1.next = mergeTwoList(l1.next, l2)
-    return l1
-  } else {
-    l2.next = mergeTwoList(l2.next, l1)
-    return l2
-  }
-}
+    //创建新链表的虚拟头节点
+    const res = new ListNode(null)
+    let cur = res
+    while (!q.isEmpty()) {
+        //取小顶堆的堆顶元素
+        const node = q.front()['element']
+        //当前节点的下个节点指向堆顶元素
+        cur.next = node
+        //当前节点后移
+        cur = cur.next
+        //堆顶元素弹出
+        q.dequeue()
+        //如果取出的堆顶元素下个节点不为空，则将下个节点入队列
+        if (node.next) q.enqueue(node.next, node.next.val)
+    }
+
+    return res.next
+};
